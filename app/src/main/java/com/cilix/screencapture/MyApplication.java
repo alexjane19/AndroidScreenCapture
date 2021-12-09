57147a603a8b4322b7916f1a58841c69ce56395b
alex <sir.alex019@gmail.com>
2021-12-09 13:28:56 +0330

    initial commit

---
+++ app/src/main/java/com/cilix/screencapture/MyApplication.java
@@ -0,0 +1,70 @@
+package com.cilix.screencapture;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.MediaRecorder;
+import android.preference.PreferenceManager;
+
+
+/**
+ * Created by AlexJane on 30-08-2018.
+ */
+public class MyApplication extends Application {
+
+
+    public static MediaRecorder mMediaRecorder;
+
+    public static final String IS_RECORDING = "is_recording";
+
+
+    private static MyApplication sInstance;
+
+
+    public static MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public static Context getAppContext() {
+        return sInstance.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        System.out.println("MyApplication");
+        sInstance = this;
+
+
+//        mDatabase = new DBMovies(this);
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, boolean preferenceValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static String readFromPreferences(Context context, String preferenceName, String defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        return sharedPreferences.getString(preferenceName, defaultValue);
+    }
+
+    public static boolean readFromPreferences(Context context, String preferenceName, boolean defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        return sharedPreferences.getBoolean(preferenceName, defaultValue);
+    }
+
+
+
+
+
+}
